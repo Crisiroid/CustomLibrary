ec62f03a9a79d9113545d158f46f4f7aa17f6923
Crisiroid <csiroid@gmail.com>
2024-08-09 23:02:53 +0330

    Add project files.

---
+++ CustomLibrary/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using CustomLibrary.Data;
+using CustomLibrary.Models;
+using CustomLibrary.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomLibrary.Controllers
+{
+    [ApiController]
+    [Route("/v1/Users")]
+    public class UserController : ControllerBase
+    {
+        private readonly LibraryDBContext _dbContext;
+        public UserController(LibraryDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("GetAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_dbContext.Users.ToList());
+        }
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser([FromBody]UserDTO user)
+        {
+            try
+            {
+                User newUser = new User
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    Password = user.Password
+                };
+                _dbContext.Users.Add(newUser);
+                _dbContext.SaveChanges();
+
+                return Ok("User Was Created");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ CustomLibrary/Data/LibraryDBContext.cs
@@ -0,0 +1,11 @@
+﻿using CustomLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomLibrary.Data
+{
+    public class LibraryDBContext : DbContext
+    {
+        public LibraryDBContext(DbContextOptions options): base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
